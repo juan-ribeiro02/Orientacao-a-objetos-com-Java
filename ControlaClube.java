ae29d8b930484fd43122cddea7724a76090026c7
juan-ribeiro02 <juan.rib02@gmail.com>
2026-04-01 10:07:11 -0300

    Add files via upload

---
+++ ControlaClube.java
@@ -0,0 +1,32 @@
+package teste;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ControlaClube {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Clube> times = new ArrayList<>();
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("Adicione o "+ (i+1) +"° time:");
+            String nomeTime = sc.nextLine();
+
+            System.out.println("numero de vitorias:");
+            int vitorias = sc.nextInt();
+
+            System.out.println("numero de empates:");
+            int empates = sc.nextInt();         
+
+            System.out.println("numero de derrotas:");
+            int derrotas = sc.nextInt();
+
+            sc.nextLine();
+
+            times.add(new Clube(nomeTime, vitorias, empates, derrotas));
+        }
+
+        sc.close();
+    }
+}
